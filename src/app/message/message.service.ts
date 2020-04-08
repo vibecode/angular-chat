a56fcaa5b4da4b735ca779542bb4440c419aff01
vibecode <rumashkin@gmail.com>
2020-04-08 22:11:42 +0200

    add messages stream

--- src/app/message/message.service.ts
+++ src/app/message/message.service.ts
@@ -2,10 +2,32 @@ import { Subject, Observable } from 'rxjs'
 import { Message } from './message.model'
 import { Thread } from '../thread/thread.model'
 import { User } from '../user/user.model'
-import { filter } from 'rxjs/operators'
+import { filter, scan, publishReplay, refCount } from 'rxjs/operators'
 
+interface IMessagesOperation extends Function {
+  (messages: Message[]): Message[]
+}
 export class MessagesService {
   newMessages: Subject<Message> = new Subject<Message>()
+  messages: Observable<Message[]>
+
+  // `updates` receives _operations_ to be applied to our `messages`
+  // it's a way we can perform changes on *all* messages (that are currently // stored in `messages`)
+  updates: Subject<any> = new Subject<any>()
+
+  constructor() {
+    this.messages = this.updates.pipe(
+      // watch the updates and accumulate operations on the messages
+      scan((messages: Message[], operation: IMessagesOperation) => {
+        return operation(messages)
+      }, initialMessages),
+      // make sure we can share the most recent list of messages across anyone
+      // who's interested in subscribing and cache the last known list of
+      // messages
+      publishReplay(1),
+      refCount()
+    )
+  }
 
   addMessage(message: Message): void {
     this.newMessages.next(message)
